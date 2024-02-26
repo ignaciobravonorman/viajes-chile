595d92a2f4c3aaec4b104ffe9ccf1f0b02f79532
hector <hector.bravonorman@gmail.com>
2024-02-26 20:31:38 -0300

    ajustes finales-termino footer

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+    $("#boton-enviar").on("click", function () {
+        alert("MENSAJE ENVIADO")
+    })
+})
